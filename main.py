4ba1ea8ca09ff51857285f23e6e6da54e3f2d4ea
Matthew Olivarez <29942482+MatthewOlivarez@users.noreply.github.com>
2021-06-13 20:34:03 -0500

    Add file via upload
    added code creating playing board

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+def printBoard(squares):
+	for i in range(3):
+		if i == 2:
+			print(' ', end='')
+		else:
+			print('_', end='')
+		for j in range(3):
+			if (i == 0 or i == 1) and j != 2:
+				print(squares[i][j] + '_|_', end='')
+			elif (i == 0 or i == 1) and j == 2:
+				print(squares[i][j] + '_', end='')
+			else:
+				if i == 2 and (j == 0 or j == 1):
+					print(squares[i][j] + ' | ', end='')
+				else:
+					print(squares[i][j] + ' ', end='')
+		print('')
+
+
 def checkWinner():
 	if ((arr[0][0] == let and arr[0][1] == let and arr[0][2] == let) or
 	   (arr[0][0] == let and arr[0][1] == let and arr[0][2] == let) or
@@ -11,7 +30,9 @@ def checkWinner():
 	else:
 		return True
 
-squares = []
+
+
+board = []
 noWinner = True
 count = 0
 
@@ -19,19 +40,10 @@ for i in range(3):
 	col = []
 	for j in range(3):
 		col.append('X')
+	board.append(col)
 
 
-for i in range(3):
-	if i == 2:
-		print(' ', end='')
-	else:
-		print('_', end='')
-	for j in range(1):
-		if i == 0 or i == 1:
-			print(' _|_ _|_ _', end='')
-		else:
-			print('  |   |   ', end='')
-	print('')
+printBoard(board)
 
 
 switcher = {
